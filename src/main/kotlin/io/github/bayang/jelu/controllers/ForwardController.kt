c528b51b7b76964ddfc84c4b8e1d8c4f61046d51
bayang <wabayang@gmail.com>
2022-03-06 21:23:34 +0100

    fix: forward controller pattern

--- src/main/kotlin/io/github/bayang/jelu/controllers/ForwardController.kt
+++ src/main/kotlin/io/github/bayang/jelu/controllers/ForwardController.kt
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
 public class ForwardController {
-    @GetMapping(value = ["/error"])
+    @GetMapping(value = ["/{path:[^\\.]*}"])
     fun forward(): String {
         return "forward:/"
     }
