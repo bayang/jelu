c923a30631298c9c37e0f142f9cb0af0d3c0c351
bayang <wabayang@gmail.com>
2024-04-24 16:49:58 +0200

    fix: use a better maintained barcode reader lib

--- src/jelu-ui/src/declarations.d.ts
+++ src/jelu-ui/src/declarations.d.ts
@@ -8,3 +8,5 @@ declare module '@kangc/v-md-editor';
 declare module '@kangc/v-md-editor/lib/theme/github.js';
 declare module '@kangc/v-md-editor/lib/lang/en-US';
 declare module '@kangc/v-md-editor/lib/preview';
+
+declare module '@teckel/vue-barcode-reader';
